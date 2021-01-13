8064e88c742ed20fb62e3f935235ca703bcd3853
Shiry1997 <krystianszuta1997@gmail.com>
2021-01-13 19:29:45 +0100

    table3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { Results3Component } from './shared/components/login/results3/results3.component';
 
 registerLocaleData(pl);
 
@@ -41,6 +42,7 @@ registerLocaleData(pl);
     LoginComponent,
     ResultsComponent,
     Results2Component,
+    Results3Component,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/results3/results3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results3',
+  templateUrl: './results3.component.html',
+  styleUrls: ['./results3.component.css']
+})
+export class Results3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
